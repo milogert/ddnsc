d24c24d5dd453c7f0aeeb604e22ca18bda32f1ae
Milo Gertjejansen <milo@milogert.com>
2015-12-01 11:35:10 -0500

    Added the add method
    As a result I also added a nice constructor for the Record class.

--- ddnsc.py
+++ ddnsc.py
@@ -1,14 +1,10 @@
 #!/bin/python2
 
 import base64
-from email.mime.text import MIMEText
-import logging
+# from email.mime.text import MIMEText
 import re
-import smtplib
-import urllib
+# import smtplib
 import urllib2
-
-from collections import defaultdict
 
 from model import engine, Record
 
@@ -20,150 +16,155 @@ session = Session()
 
 # Setup the log levels.
 class Level:
-  OFF = 0
-  LOW = 1
-  MID = 2
-  HIGH = 3
+    OFF = 0
+    LOW = 1
+    MID = 2
+    HIGH = 3
 
 logLevel = 0
 
 
-
 def setupUrl(theRows, theIp):
-  """Function to setup the url based on the rows passed in."""
+    """Function to setup the url based on the rows passed in."""
 
-  for aRow in theRows:
-    # Only update if the current ip is different from the stored one.
-    if aRow.ip != theIp:
+    for aRow in theRows:
+        # Only update if the current ip is different from the stored one.
+        if aRow.ip == theIp:
+            continue
 
-      aRow.ip = theIp
+        aRow.ip = theIp
 
-      # Format the url properly.
-      aUrl = aRow.api.format(**aRow.__dict__)
+        # Format the url properly.
+        aUrl = aRow.api.format(**aRow.__dict__)
 
-      print "Setup url as:\n\t" + aUrl
+        print "Setup url as:\n\t" + aUrl
 
-      # Update the ip with the url.
-      aPage = updateIp(aUrl, aRow.username, aRow.password)
+        # Update the ip with the url.
+        aPage = updateIp(aUrl, aRow.username, aRow.password)
 
-      # Do something with the response.
-      manageResponse(aPage, aRow)
+        # Do something with the response.
+        manageResponse(aPage, aRow)
 
 
 def updateIp(theUrl, theUser, thePass):
-  """Update the ip."""
+    """Update the ip."""
 
-  # Setup the request header.
-  request = urllib2.Request(theUrl)
+    # Setup the request header.
+    request = urllib2.Request(theUrl)
 
-  # User agent.
-  userAgent = "Python-urllib/2.6"
-  request.add_header("User-Agent", userAgent)
+    # User agent.
+    userAgent = "Python-urllib/2.6"
+    request.add_header("User-Agent", userAgent)
 
-  # Username and password, if present.
-  if theUser != "" and thePass != "":
-    base64string = base64.encodestring('%s:%s' % (theUser, thePass)).replace('\n', '')
+    # Username and password, if present.
+    if theUser == "" or thePass == "":
+        return
+
+    encode_str = '%s:%s' % (theUser, thePass)
+    base64string = base64.encodestring(encode_str).replace('\n', '')
     request.add_header("Authorization", "Basic %s" % base64string)
 
-  # Make the request.
-  response = urllib2.urlopen(request)
+    # Make the request.
+    response = urllib2.urlopen(request)
 
-  resp = response.read()
+    resp = response.read()
 
-  print("\tresponse: " + resp)
+    print("\tresponse: " + resp)
 
-  # Read the page.
-  return resp
+    # Read the page.
+    return resp
 
 
 def manageResponse(theResp, theRow):
-  """Manage the response we got from the dns provider."""
+    """Manage the response we got from the dns provider."""
 
-  good = ["good", "nochg"]
-  bad = ["nohost", "badauth", "notfqdn", "badagent", "abuse", "911"]
+    good = ["good", "nochg"]
+    bad = ["nohost", "badauth", "notfqdn", "badagent", "abuse", "911"]
 
-  # Do stuff with the results.
-  if any(s in theResp for s in good):
-    # Select all the data.
-    theRow.response = theResp
+    # Do stuff with the results.
+    if any(s in theResp for s in good):
+        # Select all the data.
+        theRow.response = theResp
 
-    # Commit the data.
-    session.add(theRow)
-    session.commit()
-  elif theResp in bad:
-    # Email the error and exit.
-    # Create the message.
-    # msg = MIMEMultipart("alternative")
+        # Commit the data.
+        session.add(theRow)
+        session.commit()
+    elif theResp in bad:
+        # Email the error and exit.
+        # Create the message.
+        # msg = MIMEMultipart("alternative")
 
-    # # Update the msg.
-    # msg["Subject"] = _myEmail["subject"].format(subdomain)
-    # msg["From"] = _myEmail["from"]
-    # msg["To"] = _myEmail["to"]
+        # # Update the msg.
+        # msg["Subject"] = _myEmail["subject"].format(subdomain)
+        # msg["From"] = _myEmail["from"]
+        # msg["To"] = _myEmail["to"]
 
-    # # Add the html to the message.
-    # msg.attach(MIMEText(_myEmail["text"].format(theResp), "plain"))
-    # msg.attach(MIMEText(_myEmail["html"].format(theResp), "html"))
+        # # Add the html to the message.
+        # msg.attach(MIMEText(_myEmail["text"].format(theResp), "plain"))
+        # msg.attach(MIMEText(_myEmail["html"].format(theResp), "html"))
 
-    # # Send the email through gmail.
-    # server = smtplib.SMTP("smtp.gmail.com:587")
-    # server.ehlo()
-    # server.starttls()
-    # server.login(_myEmail["from"], _myEmail["pass"])
-    # server.sendmail(_myEmail["from"], _myEmail["to"], msg.as_string())
-    # server.quit()
-    print("no good")
-  else:
-    # Log that this should never happen.
-    print("What happened?")
+        # # Send the email through gmail.
+        # server = smtplib.SMTP("smtp.gmail.com:587")
+        # server.ehlo()
+        # server.starttls()
+        # server.login(_myEmail["from"], _myEmail["pass"])
+        # server.sendmail(_myEmail["from"], _myEmail["to"], msg.as_string())
+        # server.quit()
+        print("no good")
+    else:
+        # Log that this should never happen.
+        print("What happened?")
 
 
 def findIp():
-  """Find the current ip of the server."""
-  request = urllib2.urlopen("https://freegeoip.net/json/").read()
-  return re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}", request)[0]
+    """Find the current ip of the server."""
+    request = urllib2.urlopen("https://freegeoip.net/json/").read()
+    return re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}", request)[0]
 
 
 def _setLogLevel(theNum):
-  global logLevel
-  logLevel = theNum
+    global logLevel
+    logLevel = theNum
 
 
 if __name__ == '__main__':
-  # Setup the argument parser.
-  import argparse
+    # Setup the argument parser.
+    import argparse
 
-  aParser = argparse.ArgumentParser(description="DDNSc")
-  aParser.add_argument("-v", "--verbose", action="count")
-  aCommand = aParser.add_mutually_exclusive_group(required=False)
-  aCommand.add_argument("-a", "--add", nargs="*")
-  aCommand.add_argument("-u", "--update", action="store_true")
-  aCommand.add_argument("-l", "--look", nargs="?", const="all")
-  
-  aArgs = aParser.parse_args()
+    aParser = argparse.ArgumentParser(description="DDNSc")
+    aParser.add_argument("-v", "--verbose", action="count")
+    aCommand = aParser.add_mutually_exclusive_group(required=False)
+    aCommand.add_argument("-a", "--add", nargs="*")
+    aCommand.add_argument("-u", "--update", action="store_true")
+    aCommand.add_argument("-l", "--look", nargs="?", const="all")
 
-  _setLogLevel(aArgs.verbose)
-  print "ii Log level is " + str(logLevel)
+    aArgs = aParser.parse_args()
 
-  if logLevel >= Level.HIGH:
-    print aArgs
+    _setLogLevel(aArgs.verbose)
+    print "ii Log level is " + str(logLevel)
 
-  if aArgs.update:
-    # Get the current ip address.
-    currentip = findIp()
+    if logLevel >= Level.HIGH:
+        print aArgs
 
-    print("ii Found ip is " + currentip)
+    if aArgs.update:
+        # Get the current ip address.
+        currentip = findIp()
 
-    setupUrl(session.query(Record).all(), currentip)
-  elif aArgs.add:
-    #aAddition = new Record(
-    pass
-  elif aArgs.look:
-    if aArgs.look == "all":
-      for aRecord in session.query(Record).all():
-        print aRecord.__dict__
+        print("ii Found ip is " + currentip)
+
+        setupUrl(session.query(Record).all(), currentip)
+    elif aArgs.add:
+        # Create a new record.
+        new_r = Record(*aArgs.add)
+
+        # Add the record to the database and commit.
+        session.add(new_r)
+        session.commit()
+    elif aArgs.look:
+        if aArgs.look == "all":
+            for aRecord in session.query(Record).all():
+                print aRecord.__dict__
+        else:
+            print aArgs.look
     else:
-      print aArgs.look
-  else:
-    aParser.print_help()
-
-
+        aParser.print_help()
--- model.py
+++ model.py
@@ -1,37 +1,54 @@
-import sqlalchemy
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+
+from config import DOMAIN, PROVIDER, API
 
 Base = declarative_base()
 
 
 class Record(Base):
-  """Class for keeping track of DNS names."""
-  __tablename__ = "Record"
+    """Class for keeping track of DNS names."""
+    __tablename__ = "Record"
 
-  id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
 
-  # Domain name stuff.
-  subdomain = Column(String(255))
-  domain = Column(String(255), nullable=False)
+    # Domain name stuff.
+    subdomain = Column(String(255))
+    domain = Column(String(255), nullable=False)
 
-  # Username/password.
-  username = Column(String(32), nullable=False)
-  password = Column(String(32), nullable=False)
+    # Username/password.
+    username = Column(String(32), nullable=False)
+    password = Column(String(32), nullable=False)
 
-  ip = Column(String(15))
+    ip = Column(String(15))
 
-  # Provider and api things.
-  provider = Column(String(255), nullable=False)
-  api = Column(String(1024))
-  extras = Column(String(1024))
+    # Provider and api things.
+    provider = Column(String(255), nullable=False)
+    api = Column(String(1024))
+    extras = Column(String(1024))
 
-  response = Column(String(1024))
+    response = Column(String(1024))
+
+    def __init__(
+            self,
+            subdomain,
+            username,
+            password,
+            extras,
+            domain=DOMAIN,
+            provider=PROVIDER,
+            api=API
+    ):
+        self.subdomain = subdomain
+        self.domain = domain
+        self.username = username
+        self.password = password
+        self.provider = provider
+        self.api = api
+        self.extras = extras
 
 
 # Lets create the database.
-engine = create_engine("sqlite:///ddnsc.db")
+engine = create_engine("sqlite:///rec.db")
 Base.metadata.create_all(engine)
-
